2c9d0534c837b657b3d0a4cd3da242915fcad583
Julien Boniol <jboniol@norsys.fr>
2016-06-02 10:48:40 +0200

    Readme et corrections présentation exceptions

--- assertj-example/README.md
+++ assertj-example/README.md
@@ -1,16 +1,37 @@
-Présentation des exemples 
+Présentation des exemples d'utilisation d'assertJ
 ===================
-Présentation
--------------------
-
+Ce projet contient différents exemples d'utilisation basiques d'assertJ
 
 Contenu du projet
 -------------------
+Le projet contient dans ses sources plusieurs classes à tester.  
+A chaque classe correspond un test (sauf pour les exceptions qui ne sont pas testées) située dans le même package que la classe testée.
+La classe de test possède le même nom que la classe testée et possède le suffixe `Test`
 
+Chaque classe contient des commentaires explicatif du fonctionnement des différents éléments.
+
+
+Les tests sont écrit au format AAA
+- Arrange : préparation des données
+- Act : lancement d'une action
+- Assert : test du résultat attendu
+
+
+Vous noterez qu'il n'y a pas de logger dans les tests : chaque test étant indépendant, on n'a pas besoin de mettre des logs.
+De plus, en cas d'erreur, les logs diront précisement ce qui ne marche pas.
 
 Utilisation
 -------------------
+Afin de pouvoir lancer les tests, vous avez deux possibilité
+- Mettez vous en ligne de commande dans le répertoire au niveau du fichier pom.xml, et lancez la commande `mvn test`
+- Dans Eclipse, clic droit sur le package src/test/java -> Run as -> JUnit test
 
+Il ne doit pas y avoir d'erreur dans les tests.
+
+Vous pouvez alors apporter des modifications dans les classes à tester pour voir le comportement des tests quand on les lance.
 
 Liens
 -------------------
+- [Projet AssertJ](http://joel-costigliola.github.io/assertj/)
+- [Sources Github du projet](https://github.com/joel-costigliola/assertj-core)
+- [Exemples d'utilisation dans le projet Github](https://github.com/joel-costigliola/assertj-examples) : les exemples sont pour les versions supportant java7 ou java8, donc pas totalement compatible avec la version java6
--- assertj-example/src/test/java/fr/norsys/tests/MethodsThrowingExceptionsTest.java
+++ assertj-example/src/test/java/fr/norsys/tests/MethodsThrowingExceptionsTest.java
@@ -17,6 +17,16 @@ import fr.norsys.tests.exceptions.RandomException;
 public class MethodsThrowingExceptionsTest {
 
     @Test
+    public void should_not_raise_any_exception_when_passing_false() {
+        MethodsThrowingExceptions method = new MethodsThrowingExceptions(74);
+
+        method.thisMethodMayFail(false);
+        // Test sans assertions...
+        // parce que c'est un void et qu'on ne peut tester que l'exception
+        // sinon, il faudrait tester le retour
+    }
+
+    @Test
     public void should_raise_RandomException_when_calling_thisMethodWillFail() {
         MethodsThrowingExceptions method = new MethodsThrowingExceptions(42);
 
@@ -40,56 +50,47 @@ public class MethodsThrowingExceptionsTest {
     }
 
     @Test
-    public void should_not_raise_any_exception_when_passing_false() {
-        MethodsThrowingExceptions method = new MethodsThrowingExceptions(74);
-
-        method.thisMethodMayFail(true);
-        // Test sans assertions...
-        // parce que c'est un void et qu'on ne peut tester que l'exception
-        // sinon, il faudrait tester le retour
-    }
-
-    @Test
     public void should_raise_RandomException_when_passing_true() {
         MethodsThrowingExceptions method = new MethodsThrowingExceptions(22);
 
-        Throwable thrownByMethod = null;
+        // autre façon de tester la levée d'exception
         try {
             method.thisMethodMayFail(true);
-        } catch (Throwable throwable) {
-            thrownByMethod = throwable;
+            // Ici, il faut faire fail parce que l'exception aurait du avoir lieu
+            failBecauseExceptionWasNotThrown(RandomException.class);
+
+            // il ne faut pas catcher Throwable en cas de fail (qui renvoie un type Error)
+        } catch (Exception thrownByMethod) {
+            assertThat(thrownByMethod)
+                    .isNotNull()
+                    .isExactlyInstanceOf(RandomException.class)
+                    .hasMessage("22 failures yet")
+                    .hasNoCause();
         }
 
-        assertThat(thrownByMethod)
-                .isNotNull()
-                .isExactlyInstanceOf(RandomException.class)
-                .hasMessage("22 failures yet")
-                .hasNoCause();
     }
 
     @Test
     public void should_raise_DiceException_when_rolling_dice() {
         MethodsThrowingExceptions method = new MethodsThrowingExceptions(21);
 
-        // autre façon de tester la levée d'exception
+        // méthode hybride entre les deux précédentes
+        Exception thrownByMethod = null;
         try {
             method.rollDice();
 
             // Ici, il faut faire fail parce que l'exception aurait du avoir lieu
             failBecauseExceptionWasNotThrown(DiceException.class);
 
-            // il ne faut pas catcher Throwable en cas de fail (qui renvoie une Error)
-        } catch (Exception thrownByMethod) {
-
-            // test de l'exception dans le catch
-            assertThat(thrownByMethod)
-                    .isInstanceOf(RandomException.class)
-                    .isExactlyInstanceOf(DiceException.class)
-                    .hasMessage("21 failures yet");
+            // il ne faut pas catcher Throwable en cas de fail (qui renvoie un type Error)
+        } catch (Exception exception) {
+            thrownByMethod = exception;
         }
 
-        // Le seul intérêt de cette méthode est d'afficher le texte "Expected %s to be thrown" au lieu de "
-        // Expecting actual not to be null
+        assertThat(thrownByMethod)
+                .isInstanceOf(RandomException.class)
+                .isExactlyInstanceOf(DiceException.class)
+                .hasMessageContaining("21 failures yet");
     }
 
 }
